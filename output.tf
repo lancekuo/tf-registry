4404341e759f928a8d4d7d0e9d0802a5fad10041
Lance Kuo <lancekuo@gmail.com>
2017-07-05 13:58:38 -0400

    Add registry-internal dns into output variables.

--- output.tf
+++ output.tf
@@ -4,3 +4,6 @@ output "secret" {
 output "access" {
     value = "${aws_iam_access_key.register_puller.id}"
 }
+output "registry_internal_dns" {
+    value = "${aws_route53_record.registry.fqdn}"
+}
